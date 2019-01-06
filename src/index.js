415da6abe692849538b43f806fcbd4bdaea94c44
Tim Hall <tim.hall.engr@gmail.com>
2019-01-06 12:01:16 -0500

    Enable html by default

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import MarkdownIt from 'markdown-it';
+import markdown from 'markdown-it';
+const default_options = { html: true };
 
-export default function md(options = 'commonmark', extend = md => md) {
+export default function md(options = default_options, extend = md => md) {
   if (typeof options === 'function') {
     extend = options;
-    options = 'commonmark';
+    options = default_options;
   }
-  const md = extend(new MarkdownIt(options));
+  const md = extend(markdown(options));
 
   return {
     markup({ content }) {
