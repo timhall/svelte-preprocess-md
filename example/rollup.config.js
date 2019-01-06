e1947f860d9bd5c425f7209dade59b2e898ac647
Tim Hall <tim.hall.engr@gmail.com>
2019-01-06 11:08:54 -0500

    Update readme

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -2,16 +2,12 @@ import svelte from 'rollup-plugin-svelte';
 import md from '../index.js';
 
 export default {
-	input: 'src/index.md',
-	output: [
-		{ file: 'dist/example.js', 'format': 'es' }
-	],
-	plugins: [
-		svelte({
-			extensions: ['.md'],
-			preprocess: {
-				markup: md()
-			}
-		})
-	]
+  input: 'src/index.md',
+  output: [{ file: 'dist/example.js', format: 'es' }],
+  plugins: [
+    svelte({
+      extensions: ['.md'],
+      preprocess: md()
+    })
+  ]
 };
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import MarkdownIt from 'markdown-it';
 export default function(options, extend = md => md) {
   const md = extend(new MarkdownIt(options));
 
-  return ({ content }) => {
-    return { code: md.render(content) };
-  }
+  return {
+    markup({ content }) {
+      return { code: md.render(content) };
+    }
+  };
 }
